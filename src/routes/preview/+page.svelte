4890aa805e2de0c3e1ba1f2f2dfc6b45d2ad897b
bulbasaur-22 <amandazm2000zm@gmail.com>
2026-06-17 11:15:01 -0400

    Add Amanda's children-exposed-to-mice bar chart

---
+++ src/routes/preview/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import AmandaStory from '$lib/components/_StoryCardTemplate_amanda.svelte';
+</script>
+
+<div style="max-width: 42rem; margin: 3rem auto; padding: 0 1.5rem;">
+	<AmandaStory />
+</div>
